618d668ba8e923212bec1596c69e8a932487f25b
Ashley <ashleyshi006@gmail.com>
2023-05-03 11:11:24 -0400

    get the PO1 Line Items

--- EDIBasicParsing.py
+++ EDIBasicParsing.py
@@ -18,6 +18,8 @@ rows = fileContents.split(rowSeperator)
 
 po850 = EDIClass.PurchaseOrder850()
 lastRefCode = ""
+firstTime = True
+foundLineItem = False
 
 for row in rows:
     # print(row)
@@ -50,6 +52,44 @@ for row in rows:
         if lastRefCode == "ST" and elementsID == "N103":
             po850.ShipToCode = el
 
+        if elementsID == "PO100":
+            if not firstTime:
+                po850.add_lineitem(po850Line)
+
+            po850Line = EDIClass.PurchaseOrder850LineItem()
+            foundLineItem = True
+            firstTime = False
+
+        if elementsID == "PO101":
+            po850Line.LineNum = el
+        if elementsID == "PO102":
+            po850Line.Qty = el
+        if elementsID == "PO103":
+            po850Line.UOM = el
+        if elementsID == "PO104":
+            po850Line.Price = el
+        if elementsID == "PO105":
+            po850Line.Basis = el
+        if elementsID == "PO107":
+            po850Line.PartNum = el
+        if elementsID == "PID05":
+            po850Line.Descr = el
+        if elementsID == "DTM02":
+            po850Line.DateRequested = el
+
+        if elementsID == "CTT01":
+            # Hand the last hanging POLine item that has not been written out yet
+            if foundLineItem:
+                po850.add_lineitem(po850Line)
+
 
 print("\n=============== End of Parsing =============")
 po850.print()
+
+
+#
+# Now you have a PO Object in memory, what do you do with it?
+# Basically, the PO needs to be loaded into your system (ERP or database)
+# 1) Serialize it to disk as XML or JSON for some other program to process
+# 2) Build SQL statements, or call Stored Procedures to store in a database
+# 3) Call an API/Library or a web Service to add the PO to your system
--- EDIClass.py
+++ EDIClass.py
@@ -11,6 +11,10 @@ class PurchaseOrder850:
         self.RefDemo = ""  # from REF*AB
         self.ShipToName = ""  # from N102
         self.ShipToCode = ""  # from N103
+        self.LineItems = []
+
+    def add_lineitem(self, lineItem):
+        self.LineItems.append(lineItem)
 
     def print(self):
         print("PONum=" + self.PONum + " PODate:" +
@@ -18,11 +22,54 @@ class PurchaseOrder850:
               self.VendorOrderNumber + " RefDemo: "+self.RefDemo +
               " ShipToCode: " + self.ShipToCode + " ShipToName: " + self.ShipToName)
 
+        for li in self.LineItems:
+            print("   " + li.LineNum + " Qty=" + str(li.Qty)+" "+li.UOM +
+                  " Price=" + str(li.Price) + " Basis=" + li.Basis +
+                  " Date:" + li.DateRequested + " PartNum=" +
+                  li.PartNum + " Descr:" + li.Descr)
+
+
+class PurchaseOrder850LineItem:
+    # handle the PO1Loop - which consists of POLoop1, PID, &DTM
+    def __init__(self):
+        self.LineNum = ""
+        self.Qty = ""
+        self.UOM = ""
+        self.Price = ""
+        self.Basis = ""
+        self.PartNum = ""
+        self.Descr = ""
+        self.DateRequested = ""
+
+    def print(self):
+        print("PONum=" + self.Po)
+
 
 # demo how to use object/class variable
 # po850 = PurchaseOrder850()
 # po850.PONum = "Test"
 # po850.POType = "NE"
 # po850.PODate = "20230501"
+
+# po850Line1 = PurchaseOrder850LineItem()
+# po850Line1.LineNum = "01"
+# po850Line1.Qty = 5
+# po850Line1.UOM = "EA"
+# po850Line1.Price = 99.97
+# po850Line1.Basis = "PE"
+# po850Line1.Descr = "Digital Widgets"
+# po850Line1.DateRequested = "20230503"
+# po850.add_lineitem(po850Line1)
+
+# po850Line2 = PurchaseOrder850LineItem()
+# po850Line2.LineNum = "02"
+# po850Line2.Qty = 8
+# po850Line2.UOM = "IN"
+# po850Line2.Price = 45.99
+# po850Line2.Basis = "PE"
+# po850Line2.Descr = "14K Gold Chain"
+# po850Line2.DateRequested = "20230502"
+# po850.add_lineitem(po850Line2)
+
 
 # po850.print()
